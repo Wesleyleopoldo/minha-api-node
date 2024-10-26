df615d533f4eec3de976ecf823858dbbd506c064
Wesley Leopoldo <wesleycauan01@gmail.com>
2024-10-26 18:05:58 -0300

    Adicionando nova rota para salvar quartos
    Signed-off-by: Wesley Leopoldo <wesleycauan01@gmail.com>

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const clientsController = require("./controllers/clientsController");
-const reservationController = require("./controllers/reservationController");
+const roomController = require("./controllers/roomController");
+// const reservationController = require("./controllers/reservationController");
 
 // Declarando a constante que tem o método de rotas do express...
 const router = express.Router();
@@ -9,7 +10,8 @@ const router = express.Router();
 router.get("/clients", clientsController.getAllClientsController);
 router.post("/clients", clientsController.createdNewClientController);
 router.delete("/clients/:id", clientsController.removeClientController);
-router.post("/reservation/:id", reservationController.createReservation);
+// router.post("/reservation/:id", reservationController.createReservation);
+router.post("/room", roomController.createRoomController);
 
 // Exportando o modulo router...
 module.exports = router;
