81c8a956810512f64b45b57cdddc68928996a3a4
Wesley Leopoldo <wesleycauan01@gmail.com>
2024-10-27 15:53:53 -0300

    cria Model para fazer a persistência dos dados no banco de dados
    Signed-off-by: Wesley Leopoldo <wesleycauan01@gmail.com>

--- src/models/reservationModel.js
+++ src/models/reservationModel.js
@@ -1,15 +1,33 @@
 const database = require("../db/db");
 
-const createReservationModel = async (request, response) => {
+const createReservationModel = async (reservation_date, client_id, room_id) => {
 
-    const reservationDate = request.reservation;
-    const clientId = request.params.clientId;
-    const roomId = request.roomId;
+    const reservationDate = reservation_date;
+    const clientId = client_id;
+    const roomId = room_id;
 
     const query = "INSERT INTO reservation(reservation_date, fk_clientId, fk_roomId) VALUES (?, ?, ?)";
 
 
-    const [newReservation] = database.execute(query, [reservationDate, checkinDate, roomId]);
+    const [createdNewReservation] = await database.execute(query, [reservationDate, clientId, roomId]);
+
+    const id = createdNewReservation.insertId;
+
+    const newReservation = await findReservationById(id);
 
     return newReservation;
 };
+
+const findReservationById = async (id) => {
+    const reservationId = id;
+
+    const query = "SELECT reservationId, reservation_date, fk_clientId, fk_roomId FROM reservation WHERE reservationId = ?";
+
+    const reservation = await database.execute(query, [reservationId]);
+
+    return reservation;
+};
+
+module.exports = {
+    createReservationModel
+}
