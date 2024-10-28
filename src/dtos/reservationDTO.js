bac5b1306e2c4235a70bc1828d5aa526d1a308ad
Wesley Leopoldo <wesleycauan01@gmail.com>
2024-10-27 21:40:16 -0300

    Cria class que gera um objeto com os dados das reservas listadas
    Signed-off-by: Wesley Leopoldo <wesleycauan01@gmail.com>

--- src/dtos/reservationDTO.js
+++ src/dtos/reservationDTO.js
@@ -1,11 +1,31 @@
 class reservationDTO {
-    constructor(reservationId, reservationDate, clientId, roomId)
+    constructor(reservationId, reservationDate, checkinDate, checkoutDate, clientId, roomId)
     {
         this.reservation_id = reservationId;
         this.reservation_date = reservationDate;
+        this.checkin_date = checkinDate;
+        this.checkout_date = checkoutDate;
         this.client_id = clientId;
         this.room_id = roomId;
     }
 };
 
-module.exports = reservationDTO;
+class allDatasOfReservationsDTO {
+    constructor(reservationId, reservationDate, checkinDate, checkoutDate, clientName, clientAddress, clientTelephone, roomType, diariesPrice)
+    {
+        this.reservationId = reservationId;
+        this.reservation_date = reservationDate;
+        this.checkin_date = checkinDate;
+        this.checkout_date = checkoutDate;
+        this.client_name = clientName;
+        this.client_address = clientAddress;
+        this.client_telephone = clientTelephone;
+        this.room_type = roomType;
+        this.diaries_price = diariesPrice;
+    }
+};
+
+module.exports = {
+    reservationDTO,
+    allDatasOfReservationsDTO
+};
