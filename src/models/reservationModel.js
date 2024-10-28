0200d2a36fbe6e74ce301338593b5447030e0ef5
Wesley Leopoldo <wesleycauan01@gmail.com>
2024-10-27 21:41:45 -0300

    Cria consulta no banco de dados que lista os dados especifícos
    Signed-off-by: Wesley Leopoldo <wesleycauan01@gmail.com>

--- src/models/reservationModel.js
+++ src/models/reservationModel.js
@@ -18,10 +18,18 @@ const createReservationModel = async (reservation_date, client_id, room_id) => {
     return newReservation;
 };
 
+const indexAllReservationModel = async () => {
+    const query = "SELECT reservation.reservationId, reservation.reservation_date, reservation.checkin_date, reservation.checkout_date, table_client.client_name, table_client.client_address, table_client.client_telephone, room.room_type, room.diaries_price FROM reservation JOIN table_client ON reservation.fk_clientId = table_client.clientId JOIN room ON reservation.fk_roomId = room.roomId";
+
+    const allReservations = await database.execute(query);
+
+    return allReservations;
+};
+
 const findReservationById = async (id) => {
     const reservationId = id;
 
-    const query = "SELECT reservationId, reservation_date, fk_clientId, fk_roomId FROM reservation WHERE reservationId = ?";
+    const query = "SELECT reservationId, reservation_date, checkin_date, checkout_date, fk_clientId, fk_roomId FROM reservation WHERE reservationId = ?";
 
     const reservation = await database.execute(query, [reservationId]);
 
@@ -29,5 +37,6 @@ const findReservationById = async (id) => {
 };
 
 module.exports = {
-    createReservationModel
+    createReservationModel,
+    indexAllReservationModel
 }
