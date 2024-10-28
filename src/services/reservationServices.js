aa29eb5ea2a777073fc07cf8a39d89feaaac4383
Wesley Leopoldo <wesleycauan01@gmail.com>
2024-10-28 00:31:12 -0300

    Adiciona função que atualiza checkin
    Signed-off-by: Wesley Leopoldo <wesleycauan01@gmail.com>

--- src/services/reservationServices.js
+++ src/services/reservationServices.js
@@ -6,7 +6,7 @@ const createReservation = async (body, params) => {
     const clientId = params.id;
     const roomId = body.roomId;
 
-    const [createdReservation] = await database.createReservationModel(reservationDate, clientId, roomId);
+    const createdReservation = await database.createReservationModel(reservationDate, clientId, roomId);
 
     const responseDTO = convertForDTO(createdReservation);
 
@@ -22,17 +22,32 @@ const indexAllReservationsServices = async () => {
     return responseDTO;
 };
 
+const updateCheckin = async (body, params) => {
+    const reservationId = body.reservationId;
+    const checkinDate = body.checkin_date;
+    const clientId = params.id;
+
+    const updatedCheckin = await database.updateCheckin(reservationId, checkinDate, clientId);
+
+    const responseDTO = convertAllDatasOfReservations(updatedCheckin);
+
+    return responseDTO;
+};
+
 function convertForDTO(reservationdata) {
     const newReservation = reservationdata.map(reservation => new reservationDTO.reservationDTO(reservation.reservationId, reservation.reservation_date, reservation.checkin_date,reservation.checkout_date, reservation.fk_clientId, reservation.fk_roomId));
     return newReservation;
-}
+};
 // Converte dados especificos para retornar no indexAllReservations...
 function convertAllDatasOfReservations(allDatas) {
+
     const allDatasOfReservations = allDatas.map(allDataReservations => new reservationDTO.allDatasOfReservationsDTO(allDataReservations.reservationId, allDataReservations.reservation_date, allDataReservations.checkin_date, allDataReservations.checkout_date, allDataReservations.client_name, allDataReservations.client_address, allDataReservations.client_telephone, allDataReservations.room_type, allDataReservations.diaries_price));
+
     return allDatasOfReservations;
 };
 
 module.exports = {
     createReservation,
-    indexAllReservationsServices
-}
+    indexAllReservationsServices,
+    updateCheckin
+};
