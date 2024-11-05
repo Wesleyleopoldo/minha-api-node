3450198da1e9f8dceb236730229b45b5ff305aa0
Wesley Leopoldo <wesleycauan01@gmail.com>
2024-11-05 19:42:05 -0300

    Adiciona novo metodo get para listar quartos
    Signed-off-by: Wesley Leopoldo <wesleycauan01@gmail.com>

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
+const urlClients = "http://localhost:3333/clients";
+const urlRooms = "http://localhost:3333/room";
+
 async function fetchUsers() {
     try {
-        const response = await axios.get("http://localhost:3333/clients"); // Certifique-se de que esta URL é a correta
+        const response = await axios.get(urlClients); // Certifique-se de que esta URL é a correta
         const users = response.data; // Aqui pegamos os dados da resposta da API
 
         const usersTable = document.getElementById("usersTable");
@@ -32,12 +35,31 @@ async function fetchUsers() {
 async function indexAllRooms() {
 
     try {
-        const response = await axios.get("http://localhost:3333/room");
-        const data = response.data;
+        const response = await axios.get(urlRooms);
+        const rooms = response.data;
 
         const roomsTable = document.getElementById("roomsTable");
+        roomsTable.innerHTML = "";
+
+        if (rooms.length === 0)
+        {
+            roomsTable.innerHTML = "<tr><td colspan='3'>Nenhum Quarto Encontrado</td></tr>";
+            return;
+        }
+
+        rooms.forEach(room => {
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+                <td>${room.roomType}</td>
+                <td>${room.diariesPrice}</td>
+            `;
+            roomsTable.appendChild(row);
+        });
     } catch (error) {
-        
+        console.error("Algo deu errado na requisição: ", error);
+        const roomsTable = document.getElementById("roomsTable");
+        roomsTable.innerHTML = "<tr><td colspan='3'>Erro ao carregar quartos</td></tr>";
     }
 }
 
@@ -45,6 +67,8 @@ async function indexAllRooms() {
 document.addEventListener("DOMContentLoaded", function(){
     if (window.location.pathname === "/indexClients.html") {
         fetchUsers();
+    } else if(window.location.pathname === "/indexRooms.html") {
+        indexAllRooms();
     } else {
         console.error("Algo deu errado...");
     }
